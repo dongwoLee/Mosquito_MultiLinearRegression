46a70ec1b28584803fa0fc5df5416aad21d818c1
dongwoLee <dongwoo4553@gmail.com>
2017-06-18 23:40:45 +0900

    modify

--- data_work.py
+++ data_work.py
@@ -1,4 +1,5 @@
 import csv
+import pandas
 import operator
 import collections
 
@@ -16,5 +17,27 @@ def csv_read(file):
 if __name__ == '__main__':
     # 2011.05.02 idx = 121
     whole_2011_list = (csv_read("All_Data_2011.csv"))
+    # a =[]
+    # for i in range(92,122):
+    #     a.append(whole_2011_list[i])
+    # pd = pandas.DataFrame(a)
+    # pd.to_csv("mosquito_2011_5.csv")
+    #
+    result_list = []# result
+    temp_list = [] # initialize
+
+    humidity = float(whole_2011_list[92][1])+float(whole_2011_list[93][1])
+    rainfall = float(whole_2011_list[92][2])+float(whole_2011_list[93][2])
+    max_tem = float(whole_2011_list[92][3])+float(whole_2011_list[93][3])
+    avg_tem = float(whole_2011_list[92][4])+float(whole_2011_list[93][4])
+    min_tem = float(whole_2011_list[92][5])+float(whole_2011_list[93][5])
+
+    temp_list.extend((humidity,rainfall,max_tem,avg_tem,min_tem))
+    result_list.append(temp_list)
+
+    for i in range
+
+
+
 
 
